699ce2767de2f2bc525c80dc99920f995d1322c8
emanmkhareez <emkhareez19@gmail.com>
2021-05-25 06:42:44 -0700

    add backend

--- server.js
+++ server.js
@@ -28,13 +28,14 @@ server.get('/weather', (req, res) => {
 
     let  city_names= weatherData.find(item => {
        
+
         if (item.city_name==city && item.lon==lon && item.lat==lat  )
 
         console.log(item.city_name);
 
             return item.city_name
     })
-    res.send(city_names )
+    
 })
 
 
@@ -60,11 +61,48 @@ server.get('/weather', (req, res) => {
 //     res.send('test file')
 
 // })
-// //when recived req not found
-// server.get('*',(req,res)=>{
-//     res.status(404).send('not found');
-// })
+
+//when recived req not found
+server.get('*',(req,res)=>{
+    res.status(404).send('not found');
+})
+// when the sever not process the requst 
+server.get('*',(req,res)=>{
+    res.status(400).send("server cannot or will not process the request");
+
+
+})
+
+//the server encountered an unexpected condition that prevented it from fulfilling the request
+server.get('*',(req,res)=>{
+    res.status(500).send("the server encountered an unexpected condition that prevented it from fulfilling the request");
+
+
+})
+
+
+let  array=[];
+var today = new Date();
+  var day = today.getDate();
 server.listen(PORT,()=>{    
     console.log(`listen on the port ${PORT}`);
 })
 
+class  ForCast
+{
+    constructor(description,date){
+    this.date=date
+    this.description=description
+
+    array.push(this);
+};
+
+   
+  
+    
+}
+let Amman=new ForCast(["Amman",35.91,31.95],day)
+let Paris=new ForCast (["Paris",2.35,48.86],day)
+let Seattle=new ForCast(["Seattle",-122.33207,47.60621],day)
+
+console.log(array);
